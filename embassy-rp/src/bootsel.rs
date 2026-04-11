a8d0b5600f6bd9d06a4e2c2967460bbdd4b3cf22
Nathan Dobson <nadobson@mac.com>
2026-04-11 13:20:03 -0700

    Initial implementation of bootsel button on rp2350.

--- embassy-rp/src/bootsel.rs
+++ embassy-rp/src/bootsel.rs
@@ -9,18 +9,21 @@
 
 use crate::Peri;
 use crate::flash::in_ram;
+use core::mem;
+use rp_pac::io::regs::GpioStatus;
 
 /// Reads the BOOTSEL button. Returns true if the button is pressed.
 ///
 /// Reading isn't cheap, as this function waits for core 1 to finish it's current
 /// task and for any DMAs from flash to complete
 pub fn is_bootsel_pressed(_p: Peri<'_, crate::peripherals::BOOTSEL>) -> bool {
-    let mut cs_status = Default::default();
+    let mut cs_status = 0u32;
 
-    unsafe { in_ram(|| cs_status = ram_helpers::read_cs_status()) }.expect("Must be called from Core 0");
-
+    unsafe { in_ram(|| cs_status = ram_helpers::read_cs_status()) }
+        .expect("Must be called from Core 0");
+    info!("{}", cs_status);
     // bootsel is active low, so invert
-    !cs_status.infrompad()
+    !unsafe { mem::transmute::<_, GpioStatus>(cs_status) }.infrompad()
 }
 
 mod ram_helpers {
@@ -38,41 +41,48 @@ mod ram_helpers {
     #[inline(never)]
     #[unsafe(link_section = ".data.ram_func")]
     #[cfg(target_arch = "arm")]
-    pub unsafe fn read_cs_status() -> GpioStatus {
-        let result: u32;
+    #[unsafe(no_mangle)]
+    pub unsafe fn read_cs_status() -> u32 {
+        let mut result: u32 = 0;
 
         // Magic value, used as both OEOVER::DISABLE and delay loop counter
-        let magic = 0x2000;
+        let magic = 0x8000;
 
         core::arch::asm!(
-            ".equiv GPIO_STATUS, 0x0",
-            ".equiv GPIO_CTRL,   0x4",
-
-            "ldr {orig_ctrl}, [{cs_gpio}, $GPIO_CTRL]",
+            ".equiv GPIO_REG, 0x1c",
+            ".equiv READ_REG, 0x08",
 
             // The BOOTSEL pulls the flash's CS line low though a 1K resistor.
             // this is weak enough to avoid disrupting normal operation.
             // But, if we disable CS's output drive and allow it to float...
-            "str {val}, [{cs_gpio}, $GPIO_CTRL]",
+            // "str {val}, [{cs_gpio2}, $GPIO_CTRL]",
+            "ldr {ctrl}, [{gpio}, $GPIO_REG]",
+            "eor {ctrl}, {ctrl}, #0x8000",
+            "and {ctrl}, {ctrl}, #0xc000",
+            "str {ctrl}, [{gpio_xor}, $GPIO_REG]",
 
             // ...then wait for the state to settle...
             "2:", // ~4000 cycle delay loop
-            "subs {val}, #8",
+            "subs {val}, #1",
             "bne 2b",
 
             // ...we can read the current state of bootsel
-            "ldr {val}, [{cs_gpio}, $GPIO_STATUS]",
+            "ldr {val}, [{gpio_read}, $READ_REG]",
 
             // Finally, restore CS to normal operation so XIP can continue
-            "str {orig_ctrl}, [{cs_gpio}, $GPIO_CTRL]",
+            "ldr {ctrl}, [{gpio}, $GPIO_REG]",
+            "and {ctrl}, {ctrl}, #0xc000",
+            "str {ctrl}, [{gpio_xor}, $GPIO_REG]",
 
-            cs_gpio = in(reg) rp_pac::IO_QSPI.gpio(1).as_ptr(),
-            orig_ctrl = out(reg) _,
+            gpio = in(reg) 0x40030000,
+            gpio_xor = in(reg) 0x40031000,
+            gpio_read = in(reg) 0xd0000000u32,
+            ctrl = out(reg) _,
             val = inout(reg) magic => result,
             options(nostack),
         );
 
-        core::mem::transmute(result)
+        result
     }
 
     #[cfg(not(target_arch = "arm"))]
