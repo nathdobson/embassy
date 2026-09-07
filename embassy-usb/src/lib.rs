dc7b565068e18c0b31b45885d092866f1f0581ce
Nathan Dobson <nadobson@mac.com>
2026-09-06 20:58:05 -0700

    rustup

--- embassy-usb/src/lib.rs
+++ embassy-usb/src/lib.rs
@@ -2,6 +2,7 @@
 #![allow(unsafe_op_in_unsafe_fn)]
 #![doc = include_str!("../README.md")]
 #![warn(missing_docs)]
+#![allow(deprecated)]
 
 // This mod MUST go first, so that the others see its macros.
 pub(crate) mod fmt;
